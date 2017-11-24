cd7b03c3040bd5788ed5eb2142caad6f1e00f485
Michael Ballantyne <michael.ballantyne@gmail.com>
2017-11-23 19:15:26 -0500

    bootfile

--- bootfiles/nodecli.js
+++ bootfiles/nodecli.js
@@ -191,15 +191,73 @@
         return { main: main }
     }
     )(flatten, resolve);
-    const runner = (// require:
-    // provide:
-    function () {
-        return {}
+    const runner = (// require: compilejs
+    // provide: run
+    function (compilejs) {
+        const subset = function (left, right) {
+            for (var elem of left) {
+                if (!right.has(elem)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    
+        const run = function(resolve, eval_module, module_name) {
+            if (!(typeof module_name === "string" || module_name instanceof String)) {
+                throw "malformed module name; should be a string: " + module_name;
+            }
+    
+            const run_module_internal = function (instance_map, module_name) {
+                const existing_instance = instance_map[module_name];
+                if (existing_instance !== undefined) {
+                    return existing_instance;
+                }
+    
+                const module_source = resolve(module_name);
+                const module_declaration = compilejs.compileJS(module_source);
+    
+                const imports = module_declaration.imports
+    
+                const imports_instantiated =
+                    imports.reduce(run_module_internal, instance_map);
+    
+                const body_function = eval_module(module_declaration.body_code);
+                const instance =
+                    body_function.apply(undefined, imports.map(i => imports_instantiated[i]));
+    
+                if (!subset(new Set(module_declaration.exports), new Set(Object.keys(instance)))) {
+                    throw "Module instance does not include all keys listed in exports: " + module_name;
+                }
+    
+                return {
+                    ...imports_instantiated,
+                    [module_name]: instance
+                }
+            }
+    
+            return run_module_internal({}, module_name)[module_name];
+        }
+    
+        return { run: run}
     }
-    )();
-    const nodecli = (// require: resolve, bootstrap, runner
+    )(compilejs);
+    const nodecli = (// require: bootstrap, runner
     // provide: main
-    function (resolve, bootstrap, runner) {
+    function (bootstrap, runner) {
+        const fs = require("fs")
+        const vm = require("vm")
+    
+        function resolve(name) {
+            const text = fs.readFileSync("modules/" + name + ".js").toString();
+    
+            return text;
+        }
+    
+        function eval_module(text) {
+            return vm.runInNewContext(text, {console: console, require: require, process: process})
+        }
+    
         function usage() {
             console.log("Usage: node run.js --bootstrap | --run <module-name> <function>");
             process.exit(1);
@@ -207,18 +265,17 @@
     
         function main(args) {
             if (args[0] === "--run") {
-                throw "not implemented2"
-            }
-            if (args[0] === "--bootstrap") {
+                const module_instance = runner.run(resolve, eval_module, args[1])
+                module_instance["main"](args.slice(2));
+            } else if (args[0] === "--bootstrap") {
                 bootstrap.main([])
-            }
-            else {
+            } else {
                 usage()
             }
         }
     
         return { main: main }
     }
-    )(resolve, bootstrap, runner);
+    )(bootstrap, runner);
     return nodecli;
 });
