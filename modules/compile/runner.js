ee23abbd43d8996786f6b74561c851c449af569e
Michael Ballantyne <michael.ballantyne@gmail.com>
2018-08-04 18:29:59 -0400

    avoid redeclaration of modules given repeated loads for parsing and #langs

--- modules/compile/runner.js
+++ modules/compile/runner.js
@@ -3,10 +3,20 @@
 // provide: make_runner
 (function (Immutable, lang) {
     function make_runner(platform) {
+        var declaration_cache = Immutable.Map()
+
         function load(module_name) {
-            const module_source = platform.resolve(module_name);
-            const module_declaration = lang.compile_via_lang(module_source, {load: load, run: run});
-            return module_declaration;
+            if (declaration_cache.has(module_name)) {
+                return declaration_cache.get(module_name);
+            } else {
+                const module_source = platform.resolve(module_name);
+                const module_declaration = lang.compile_via_lang(module_source, {load: load, run: run});
+
+                module_declaration.body_function = platform.eval_module(module_declaration.body_code);
+
+                declaration_cache = declaration_cache.set(module_name, module_declaration);
+                return module_declaration;
+            }
         }
 
         function run(module_name) {
@@ -21,14 +31,11 @@
 
                 const module_declaration = load(module_name)
 
-                const imports = module_declaration.imports
-
                 const imports_instantiated =
-                    imports.reduce(run_module_internal, instance_map);
+                    module_declaration.imports.reduce(run_module_internal, instance_map);
 
-                const body_function = platform.eval_module(module_declaration.body_code);
                 const instance =
-                    body_function.apply(undefined, imports.map(i => imports_instantiated.get(i)));
+                    module_declaration.body_function.apply(undefined, module_declaration.imports.map(i => imports_instantiated.get(i)));
 
                 if (!(Immutable.Collection(module_declaration.exports).isSubset(Immutable.Collection(Object.keys(instance))))) {
                     throw "Module instance does not include all keys listed in exports: " + module_name;
